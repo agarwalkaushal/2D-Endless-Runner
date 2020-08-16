e27b2bc5e0c8abffb76da23e1da34823e08d2f88
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-08-16 13:45:04 +0530

    Step 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float dir;
+    int speed = 3;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) dir = 1;
+        if (Input.GetKeyDown(KeyCode.A)) dir = -1;
+        rb2d.velocity = new Vector2(dir * speed, 0);
+    }
+}
