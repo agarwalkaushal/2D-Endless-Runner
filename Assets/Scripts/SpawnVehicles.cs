a91ed94c6d5f0040092dc8d5ff00d39e592c0651
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-08-22 14:57:38 +0530

    Step 3

---
+++ Assets/Scripts/SpawnVehicles.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnVehicles : MonoBehaviour
+{
+    public Sprite[] vehicleSprites;
+    public GameObject vehiclePrefab;
+
+    float spawnTime = 0f;
+    float spawnDelay = 1f;
+    int speed = -10;
+    
+    void Start()
+    {
+        InvokeRepeating("spawnVehicle", spawnTime, spawnDelay);
+    }
+
+    void spawnVehicle()
+    {
+        int i = Random.Range(0, vehicleSprites.Length);
+        int pos = Random.Range(0, 2);
+        Quaternion rotation = Quaternion.Euler(180, 0, 0);
+
+        GameObject vehicle = Instantiate(vehiclePrefab, new Vector3(pos == 0 ? -1.5f : 1.5f, 14f, 0), rotation);
+
+        SpriteRenderer img = vehicle.GetComponent<SpriteRenderer>();
+        img.sprite = vehicleSprites[i];
+
+        Rigidbody2D rb = vehicle.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0.0f, speed);
+    }
+}
+
